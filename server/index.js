2447090223beb3f7f2a6b3e77ba1123c6a1f860e
agiz <ziga.zupanec@gmail.com>
2018-12-09 21:10:31 +0100

    Updated API.

--- server/index.js
+++ server/index.js
@@ -180,6 +180,88 @@ apiRoutes.use(async (req, res, next) => {
   }
 })
 
+// get all keywords (for auto complete purposes)
+apiRoutes.get('/keywords', async (req, res) => {
+  console.log("(route) GET /keywords")
+
+  const values = await pgClient.query('SELECT DISTINCT keyword FROM pin_crawl;')
+  res.json(values.rows)
+})
+
+// get all pins matching the keyword
+apiRoutes.post('/search', async (req, res) => {
+  console.log("(route) GET /search")
+
+  const cutoff_id = 'id' in req.body ? `id < ${req.body.id} AND` : ''
+  // TODO: check for injections
+
+  const term = 'term' in req.body ? req.body.term : ''
+  // TODO: check for injections
+
+  const sql = `SELECT t1.id, t1.pin_id, t1.profile_id, t1.saves, t1.repin_count, t1.created_at, t1.last_repin_date, t1.keyword, t1.position, t1.crawled_at
+    FROM pin_crawl t1
+    JOIN (
+        SELECT pin_id, max(id) AS max_id
+        FROM pin_crawl
+        WHERE
+        ${cutoff_id}
+        keyword ILIKE '%${term}%'
+        GROUP BY pin_id
+    ) t2
+    ON t1.id = t2.max_id
+    ORDER BY t1.id DESC
+    FETCH FIRST 100 ROWS ONLY;
+  `
+  // This sql query first retrieves only unique entries, then filters them by keyword and offsets them from id.
+
+  // const sql = `SELECT id, pin_id, profile_id, saves, repin_count, created_at, last_repin_date, keyword, position, crawled_at FROM pin_crawl WHERE ${cutoff_id} keyword ILIKE '%${term}%' ORDER BY id DESC FETCH FIRST 100 ROWS ONLY;`
+
+  const values = await pgClient.query(sql)
+  res.json(values.rows)
+})
+
+// get all profiles (match with ads for country, user agent, gender...)
+apiRoutes.get('/profiles', async (req, res) => {
+  console.log("(route) GET /profiles")
+
+  const values = await pgClient.query('SELECT profile.id, profile.gender, profile.country_code, profile.interest, user_agent.user_agent FROM profile LEFT JOIN user_agent ON profile.user_agent_id = user_agent.id;')
+  res.json(values.rows)
+})
+
+// get pin(s) by id (by an array of ids)
+apiRoutes.post('/pin', async (req, res) => {
+  console.log("(route) POST /pin")
+
+  const ids = req.body.map(x => `'${x}'`).join(',')
+  const sql = `SELECT id, promoter_id, description, ad_url, image, mobile_link, is_video, title, is_shopify FROM pin WHERE id IN (${ids});`
+
+  const values = await pgClient.query(sql)
+  res.json(values.rows)
+})
+
+// get promoter(s) by id (by an array of ids)
+apiRoutes.post('/promoter', async (req, res) => {
+  console.log("(route) POST /promoter")
+
+  const ids = req.body.map(x => `'${x}'`).join(',')
+  const sql = `SELECT id, username, location, external_url, description, image FROM promoter WHERE id IN (${ids});`
+
+  const values = await pgClient.query(sql)
+  res.json(values.rows)
+})
+
+// get promoter details by id (by an array of ids)
+// get latest details for the current month of requested provider
+apiRoutes.post('/promoterdetails', async (req, res) => {
+  console.log("(route) POST /promoterdetails")
+
+  const ids = req.body.map(x => `'${x}'`).join(',')
+  const sql = `SELECT DISTINCT ON (promoter_id) promoter_id, followers, monthly_views FROM promoter_crawl WHERE promoter_id IN (${ids}) ORDER BY promoter_id, crawled_at DESC;`
+
+  const values = await pgClient.query(sql)
+  res.json(values.rows)
+})
+
 apiRoutes.get('/ads', async (req, res) => {
   console.log("(route) GET /ads")
 
